3ce1563b7309ed79b6934a4e3543e7449a8b9298
anthony-ng <thomsonng123@gmail.com>
2015-02-03 14:33:03 -0800

    updated seed.rb with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+10.times do
+  Question.create(title: Faker::Hacker.say_something_smart + "?")
+end
