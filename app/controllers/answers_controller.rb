58155207734b199ab1661698ee8e6645adcc2a35
anthony-ng <thomsonng123@gmail.com>
2015-02-04 15:55:36 -0800

    updated answer controller & form: able to add new answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,16 @@
 class AnswersController < ApplicationController
 
   def create
-    @question = Question.find(params[:id])
-    @answer = @question.new(answer_params)
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.new(answer_params)
     @answer.save
+
+    redirect_to question_path(@question)
   end
 
   private
 
   def answer_params
-    params.require(:answer).permit(:content)
+    params.require(:answer).permit(:title, :content)
   end
 end
