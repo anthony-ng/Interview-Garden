65f2ba9f54da7ba644ea92513f8ac3bc86568175
anthony-ng <thomsonng123@gmail.com>
2015-02-04 10:34:28 -0800

    refactor show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get "questions/test" => "questions#test"
 
   root :to => "questions#index"
+
   resources :questions do
     resources :answers
   end
+
+  get '/all' => 'questions#index', as: 'all_questions'
+
 end
