7d3b3a8626a14c72d8495f706a73763b83a676e3
anthony-ng <thomsonng123@gmail.com>
2015-02-04 15:04:00 -0800

    added answer contoller & strong params for create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,16 @@
 class AnswersController < ApplicationController
   def new
+    @answer = Answer.new
+  end
+
+  def create
+    @answer = Answer.new(answer_params)
+    @answer.save
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:title, :content)
   end
 end
