82559869342996dab844144781ef307f10f0b71a
anthony-ng <thomsonng123@gmail.com>
2015-02-04 13:26:10 -0800

    added confirmation for new form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,6 +17,8 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(params[:question])
     @question.save
+
+    redirect_to :action => :index
   end
 
   def delete
