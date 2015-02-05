f58ab0e50263a2471b30378c7bb4aedc45186d2d
anthony-ng <thomsonng123@gmail.com>
2015-02-05 15:26:50 -0800

    fixed delete route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -32,7 +32,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.destroy
 
-    redirect_to :action => :index
+    redirect_to questions_path
   end
 
   def upvote
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205202253) do
+ActiveRecord::Schema.define(version: 20150205222631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20150205202253) do
     t.string   "title"
     t.text     "content"
     t.integer  "question_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "vote",        default: 0
   end
 
   create_table "questions", force: :cascade do |t|
