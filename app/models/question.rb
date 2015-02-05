c5987188d3aa1f09afd5c26916a9e37db2cbe6ce
anthony-ng <thomsonng123@gmail.com>
2015-02-05 12:19:38 -0800

    added default_scope to latest created_at

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ActiveRecord::Base
   validates :title, :content, :vote_count, presence: true
   has_many :answers
-  default_scope {order("created_at DESC")}
+  default_scope {
+    order("created_at DESC")
+  }
 end
