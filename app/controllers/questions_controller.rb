bca746d91e86dec947f762cd558839bfedd930bb
anthony-ng <thomsonng123@gmail.com>
2015-02-05 12:12:24 -0800

    updated no answer error handling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,15 +4,13 @@ class QuestionsController < ApplicationController
   def get_question
     @question = Question.find(params[:id])
   end
-
+#############################################
   def index
     @questions = Question.all
   end
 
   def show
-    if @question.answers.count == 0
-      @empty = "Sorry, no one answers yet!"
-    end
+      @empty = "Sorry, no answer yet!" if @question.answers.count == 0
   end
 
   def new
@@ -50,7 +48,7 @@ class QuestionsController < ApplicationController
     # @question = Question.find(params[:id])
     @question.decrement!(:vote_count)
   end
-
+#############################################
   private
 
   def question_params
