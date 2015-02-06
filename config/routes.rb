68867a7c5e692d9570c440237271b57eeef4e842
anthony-ng <thomsonng123@gmail.com>
2015-02-05 16:33:05 -0800

    updated answer vote routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'answers/new'
-
   root :to => "questions#index"
 
-  # Nested routes
   resources :questions do
-    resources :answers
     member do
       post 'upvote'
       post 'downvote'
     end
-  end
 
-  # get '/questions/:id/upvote' => 'questions#upvote'
+    # Nested routes
+    resources :answers do
+      member do
+        post 'upvote'
+        post 'downvote'
+      end
+    end
+  end
 
   # Custom routes
   get '/all' => 'questions#index', as: 'all_questions'
