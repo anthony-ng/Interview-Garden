f608c5223b4e1f60591c14096b71ce2a6b0ae2c3
anthony-ng <thomsonng123@gmail.com>
2015-02-06 16:19:48 -0800

    jQuery working

--- app/assets/javascripts/question.js
+++ app/assets/javascripts/question.js
@@ -1,42 +1,44 @@
 $(document).ready(function() {
 
   ListenForQuestionVote()
-  // ListenForAnswerVote()
-  // ListenForQuestionForm()
-  // ListenForAnswerForm()
+  ListenForAnswerVote()
+  ListenForQuestionForm()
+  ListenForAnswerForm()
 
 });
 
 function ListenForQuestionVote() {
   $(".voting").on("click", function(event){
     event.preventDefault();
-    console.log("Inside ListenForQuestionVote function");
+    alert("Inside ListenForQuestionVote function");
 
   })
-}
+};
 
 console.log("Outside!")
 
 function ListenForAnswerVote() {
   $(".answer_voting").on("click", function(event){
-    event.preventDefault();
-    console.log("Inside ListenForAnswerVote function");
+    // event.preventDefault();
+    alert("Inside ListenForAnswerVote function");
 
   })
-}
+};
 
 function ListenForQuestionForm() {
-  $("#create_question").on("submit", function(event){
-    event.preventDefault();
+  $("#create_question").on("click", function(event){
+    // event.preventDefault();
+    alert("Inside ListenForQuestionForm function");
     console.log("Inside ListenForQuestionForm function");
 
   })
-}
+};
 
 function ListenForAnswerForm() {
   $("#create_answer").on("submit", function(event){
-    event.preventDefault();
+    // event.preventDefault();
+    alert("Inside ListenForAnswerForm function");
     console.log("Inside ListenForAnswerForm function");
 
   })
-}
+};
