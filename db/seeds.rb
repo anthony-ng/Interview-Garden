b1ef485f8375d61da46d09872f1e817111789be4
anthony-ng <thomsonng123@gmail.com>
2015-02-07 00:21:37 -0800

    updated seeding with new questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,37 +8,26 @@ require 'faker'
 # end
 
 
-Question.create(title: "Writing interview questions hasn't made me rich. Maybe trading Apple stocks will.", content: 'I have an array stock_prices_yesterday where:
-
-The indices are the time, as a number of minutes past trade opening time, which was 9:30am local time.
-The values are the price of Apple stock at that time, in dollars.
-For example, the stock cost $500 at 10:30am, so stock_prices_yesterday[60] = 500.
-
-Write an efficient algorithm for computing the best profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday. For this problem, we won\'t allow "shorting"—you must buy before you sell.')
-Question.create(title: "You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.", content: "Write a function get_products_of_all_ints_except_at_index() that takes an array of integers and returns an array of the products.")
-Question.create(title: "Given an array_of_ints, find the highest_product you can get from three of the integers.", content: "The input array_of_ints will always have at least three integers.")
-Question.create(title: "Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available.", content: "To do this, you’ll need to know when any team is having a meeting. In HiCal, a meeting is stored as a tuple of integers (start_time, end_time) . These integers represent the number of 30-minute blocks past 9:00am.")
-Question.create(title: "Imagine you landed a new job as a cashier...", content: "Your quirky boss found out that you're a programmer and has a weird request about something they've been wondering for a long time.
-
-Write a function that, given:
-
-an amount of money
-a list of coin denominations
-computes the number of ways to make amount of money with coins of the available denominations.")
-Question.create(title: "A crack team of love scientists from OkEros (a hot new dating site) have devised a way to represent dating profiles as rectangles on a two-dimensional plane.", content: "They need help writing an algorithm to find the intersection of two users' love rectangles. They suspect finding that intersection is the key to a matching algorithm so powerful it will cause an immediate acquisition by Google or Facebook or Obama or something.")
-Question.create(title: "You decide to test if your oddly-mathematical heating company is fulfilling its All-Time Max, Min, Mean and Mode Temperature Guarantee™.", content: "Write a class TempTracker with these methods:
-
-insert()—records a new temperature
-get_max()—returns the highest temp we've seen so far
-get_min()—returns the lowest temp we've seen so far
-get_mean()—returns the mean ↴ of all temps we've seen so far
-get_mode()—returns the mode ↴ of all temps we've seen so far")
-Question.create(title: 'Write a function to see if a binary tree ↴ is "superbalanced" (a new tree property we just made up).', content: 'A tree is "superbalanced" if the difference between the depths of any two leaf nodes is no greater than one.')
-Question.create(title: 'Write a function to check that a binary tree ↴ is a valid binary search tree ↴ .', content: 'As title')
-Question.create(title: 'Write a function to find the 2nd largest element in a binary search tree ↴ .', content: 'As title')
-Question.create(title: 'Write a function to find the 2nd largest element in a binary search tree ↴ .', content: 'As title')
-Question.create(title: 'Suppose we had an array ↴ of n integers in sorted order. How quickly could we check if a given integer is in the array?', content: 'As title')
-# g
+Question.create(title: 'Apple Stocks', content: 'I have an array stock_prices_yesterday where:The indices are the time, as a number of minutes past trade opening time, which was 9:30am local time.The values are the price of Apple stock at that time, in dollars.')
+Question.create(title: 'Product of All Other Numbers', content: 'Write a function get_products_of_all_ints_except_at_index() that takes an array of integers and returns an array of the products.')
+Question.create(title: 'Highest Product of 3', content: 'Given an array_of_ints, find the highest_product you can get from three of the integers.')
+Question.create(title: 'Making Change', content: 'Your quirky boss found out that you\'re a programmer and has a weird request about something they\'ve been wondering for a long time. Write a function that, given:an amount of money a list of coin denominations computes the number of ways to make amount of money with coins of the available denominations.')
+Question.create(title: 'Temperature Tracker', content: 'Write a class TempTracker with these methods: insert()—records a new temperature get_max()—returns the highest temp we\'ve seen so far get_min()—returns the lowest temp we\'ve seen so far get_mean()—returns the mean of all temps we\'ve seen so far get_mode()—returns the mode of all temps we\'ve seen so far')
+Question.create(title: 'Balanced Binary Tree', content: 'Write a function to see if a binary tree  is "superbalanced" (a new tree property we just made up).')
+Question.create(title: 'Binary Search Tree Checker', content: 'Write a function to check that a binary tree is a valid binary search tree .')
+Question.create(title: '2nd Largest Item in a Binary Search Tree', content: 'Write a function to find the 2nd largest element in a binary search tree.')
+Question.create(title: 'Find in Ordered Set', content: 'Suppose we had an array of n integers in sorted order. How quickly could we check if a given integer is in the array?')
+Question.create(title: 'Compute nth Fibonacci Number', content: 'Write a function fib() that a takes an integer n and returns the nth fibonacci number.')
+Question.create(title: 'Queue Two Stacks', content: 'Implement a queue with 2 stacks . Your queue should have an enqueue and a dequeue function and it should be "first in first out" (FIFO).')
+Question.create(title: 'Delete Node', content: 'Delete a node from a singly linked list , given only a variable pointing to that node.')
+Question.create(title: 'Reverse A Linked List', content: 'Write a function for reversing a linked list .')
+Question.create(title: 'Reverse String in Place', content: 'Write a function to reverse a string in place.')
+Question.create(title: 'Reverse Words', content: 'Write a function reverse_words() that takes a string message and reverses the order of the words in place .')
+Question.create(title: 'Permutation Palindrome', content: 'Write an efficient function that checks whether any permutation of an input string is a palindrome.')
+Question.create(title: 'Recursive String Permutations', content: 'Write a recursive function for generating all permutations of an input string. Return them as an array.')
+Question.create(title: 'Merge Sorted Arrays', content: 'We have our lists of orders sorted numerically already, in arrays. Write a function to merge our arrays of orders into one sorted array.')
+Question.create(title: 'Two Egg Problem', content: 'If an egg is dropped from above that floor, it will break. If it is dropped from that floor or below, it will be completely undamaged and you can drop the egg again. Given two eggs, find the highest floor an egg can be dropped from without breaking, with as few drops as possible.')
+Question.create(title: 'Simulate 5-sided die', content: 'You have a function rand7() that generates a random number from 1 to 7. Use it to write a function rand5() that generates a random number from 1 to 5.')
 
 50.times do
   Answer.create(title: Faker::Hacker.say_something_smart, content: Faker::Lorem.sentences(5).join(" "), question_id: rand(1..20))
